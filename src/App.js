1b5be769adff61b1e43ab6ff5842bde744c84d38
meriemkhadraoui <miyakhad@gmail.com>
2021-06-14 14:43:21 +0200

    online store

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import Data from "./data.json";
+import React, { useState } from "react";
 function App() {
-  return <div className="App">my online shop</div>;
+  const [userInput, setUserInput] = useState("");
+
+  const changeHandle = (e) => {
+    setUserInput(e.target.value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <React.Fragment>
+      <h2>Welcom to NiNa shop</h2>
+      <form onSubmit={handleSubmit}>
+        <input type="text" onChange={changeHandle} value={userInput} />
+        <input type="submit" value="search" />
+      </form>
+    </React.Fragment>
+  );
 }
 
 export default App;
