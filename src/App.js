7312d09e9964906e0ec050667ab1455b42dd8803
meriemkhadraoui <miyakhad@gmail.com>
2021-06-23 13:53:54 +0200

    use context hook

--- src/App.js
+++ src/App.js
@@ -3,10 +3,24 @@ import PorductList from "./components/ProsectList";
 import { Store, storContext } from "./context";
 
 function App() {
+  const [userInput, setUserInput] = useState("");
+  // const [filteredData, setFilteredData] = useState([]);
+  const changeHandle = (e) => {
+    setUserInput(e.target.value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    Store.lookUp(userInput);
+  };
+  // console.log(filteredData);
   return (
     <storContext.Provider value={Store}>
       <h1>welcome to NiNa shop</h1>
-      <PorductList />
+      <form onSubmit={handleSubmit}>
+        <input type="text" onChange={changeHandle} value={userInput} />
+        <input type="submit" value="filter" />
+      </form>
+      <PorductList data={Store.lookUp(userInput)} />
     </storContext.Provider>
   );
 }
--- src/context/index.js
+++ src/context/index.js
@@ -7,6 +7,14 @@ const Store = {
     return this.cart.reduce((acc, cur) => acc + cur.price, 0);
   },
   addToCart: (item) => Store.cart.push(item),
+  lookUp: (str) => {
+    let userText = str.toLocaleLowerCase().trim();
+    let newArrLength = userText.length;
+    return Store.data.filter((item) => {
+      const slicedProductName = item.productName.slice(0, newArrLength);
+      return slicedProductName === userText;
+    });
+  },
 };
 const storContext = React.createContext(Store);
 export { Store, storContext };
